5a83b8150532df6ab493a26132868574096db320
Ndelforno <nicolas.delforno@gmail.com>
2018-09-12 14:17:23 -0400

    exercise done

---
+++ exercise.rb
@@ -0,0 +1,39 @@
+# emotions = {
+#   :happiness => rand(1..3),
+#   :fear => rand(1..3),
+#   :sadness => rand(1..3),
+#   :anxiousness => rand(1..3)
+# }
+
+#readers
+
+
+class Person
+
+#instance methods
+  def initialize(name)
+    @name = name
+    @emotions = {
+      :happiness => rand(1..3),
+      :fear => rand(1..3),
+      :sadness => rand(1..3),
+      :anxiousness => rand(1..3)
+    }
+  end
+
+  def emotions
+    return @emotions
+  end
+
+  def feelings(person)
+    @emotions.each do |emotion,level|
+    puts "#{@name} is feeling a #{level} amount of #{emotion}"
+    end
+  end
+end
+
+person1 = Person.new("Nico")
+person2 = Person.new("David")
+#puts person1.inspect
+puts person1.feelings(person1)
+puts person2.feelings(person2)
